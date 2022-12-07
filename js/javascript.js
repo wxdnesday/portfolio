0ba3e1415b67847da4702e36c31362fc10800031
wxdnesday <todayis.wednesday111@gmail.com>
2022-12-06 23:49:00 -0800

    updating for transitions

--- js/javascript.js
+++ js/javascript.js
@@ -53,3 +53,15 @@ window.addEventListener('scroll', function(){
     // }
 
 })
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    })
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
